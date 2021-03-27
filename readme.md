adb036ea5f61f26537081332e742cc01c6ffadd0
Ariel Davis <ariel.z.davis@icloud.com>
2021-03-27 01:30:51 -0700

    Start

---
+++ readme.md
@@ -0,0 +1,14 @@
+# try-lean
+
+Trying out the [Lean][1] theorem prover, mostly to prove properties about
+programming languages.
+
+[1]: https://leanprover.github.io
+
+## Note about the Lean version
+
+This uses Lean 3.4.2, even though the 'latest' version as of writing is Lean 4.
+This is because the docs for Lean 4 seem even more scarce than the docs for Lean
+3, and I couldn't figure out how to do some stuff. In general, although Lean 4
+is clearly the future, it seems like it's not totally ready for prime time yet.
+I'm excited for the future though!
---
+++ src/e.lean
@@ -0,0 +1,72 @@
+-- The E programming language.
+
+inductive exp: Type
+| zero: exp
+| succ: exp -> exp
+| true: exp
+| false: exp
+
+inductive typ: Type
+| nat: typ
+| bool: typ
+
+inductive has_typ: exp -> typ -> Prop
+| zero: has_typ exp.zero typ.nat
+| succ (e: exp): has_typ e typ.nat -> has_typ (exp.succ e) typ.nat
+| true: has_typ exp.true typ.bool
+| false: has_typ exp.false typ.bool
+
+theorem inversion_succ
+  (e: exp) (t: typ)
+  (typing: has_typ (exp.succ e) t)
+  : has_typ e t ∧ t = typ.nat :=
+begin
+  cases typing,
+  split,
+  exact typing_a,
+  refl,
+end
+
+inductive val: exp -> Prop
+| zero: val exp.zero
+| succ (e: exp): val e -> val (exp.succ e)
+| true: val exp.true
+| false: val exp.false
+
+inductive steps: exp -> exp -> Prop
+| succ (e: exp) (e': exp): steps e e' -> steps (exp.succ e) (exp.succ e')
+
+theorem preservation
+  (e: exp) (e': exp) (t: typ)
+  (typing: has_typ e t)
+  (stepping: steps e e')
+  : has_typ e' t :=
+begin
+  induction stepping,
+  let inv := (inversion_succ stepping_e t typing),
+  let left := inv.left,
+  rewrite inv.right at *,
+  apply has_typ.succ stepping_e',
+  exact stepping_ih left,
+end
+
+theorem progress
+  (e: exp) (t: typ)
+  (typing: has_typ e t)
+  : val e ∨ (∃ (e': exp), steps e e') :=
+begin
+  induction typing,
+  left,
+  exact val.zero,
+  cases typing_ih,
+  left,
+  exact val.succ typing_e typing_ih,
+  right,
+  cases typing_ih,
+  existsi exp.succ typing_ih_w,
+  exact steps.succ typing_e typing_ih_w typing_ih_h,
+  left,
+  exact val.true,
+  left,
+  exact val.false,
+end
