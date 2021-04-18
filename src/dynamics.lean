2d56d735c17840d72786c4a399182c96283925d2
Ariel Davis <ariel.z.davis@icloud.com>
2021-04-17 17:34:56 -0700

    Add a var cx

--- src/dynamics.lean
+++ src/dynamics.lean
@@ -1,13 +1,20 @@
 import syntax
 
-inductive steps: exp -> exp -> Prop
+inductive vcx: Type
+| nil: vcx
+| cons: vcx -> var -> val -> vcx
+
+inductive steps: vcx -> exp -> exp -> Prop
 | if_e1
+    (Δ: vcx)
     (e1: exp) (e1': exp) (e2: exp) (e3: exp):
-    steps e1 e1' ->
-    steps (exp.if_ e1 e2 e3) (exp.if_ e1' e2 e3)
+    steps Δ e1 e1' ->
+    steps Δ (exp.if_ e1 e2 e3) (exp.if_ e1' e2 e3)
 | if_true
+    (Δ: vcx)
     (e2: exp) (e3: exp):
-    steps (exp.if_ (exp.pure val.true) e2 e3) e2
+    steps Δ (exp.if_ (exp.pure val.true) e2 e3) e2
 | if_false
+    (Δ: vcx)
     (e2: exp) (e3: exp):
-    steps (exp.if_ (exp.pure val.false) e2 e3) e3
+    steps Δ (exp.if_ (exp.pure val.false) e2 e3) e3
--- src/safety.lean
+++ src/safety.lean
@@ -3,42 +3,45 @@ import helpers
 -- seems like we have to write this as taking in a general context and also a
 -- proof that it is nil, rather than just having `et: has_typ cx.nil e tau`.
 theorem progress
-  (Γ: cx)
+  (Γ: cx) (Δ: vcx)
+  (cx_nil: Γ = cx.nil)
+  (vcx_nil: Δ = vcx.nil)
   (e: exp) (τ: typ)
-  (is_nil: Γ = cx.nil)
   (et: exp_typ Γ e τ)
-  : (∃ (v: val), e = exp.pure v) ∨ (∃ (e': exp), steps e e') :=
+  : (∃ (v: val), e = exp.pure v) ∨ (∃ (e': exp), steps Δ e e') :=
 begin
   induction et,
   left,
   existsi et_v,
   refl,
   right,
-  cases et_ih_a is_nil,
+  cases et_ih_a cx_nil,
   cases h,
   rewrite h_h at *,
   cases bool_canonical_forms et_Γ h_w (inversion_pure et_Γ h_w typ.bool et_a),
   rewrite h,
   existsi et_e2,
-  exact steps.if_true et_e2 et_e3,
+  exact steps.if_true Δ et_e2 et_e3,
   rewrite h,
   existsi et_e3,
-  exact steps.if_false et_e2 et_e3,
+  exact steps.if_false Δ et_e2 et_e3,
   cases h,
   existsi exp.if_ h_w et_e2 et_e3,
-  exact steps.if_e1 et_e1 h_w et_e2 et_e3 h_h,
+  exact steps.if_e1 Δ et_e1 h_w et_e2 et_e3 h_h,
 end
 
 theorem preservation
-  (Γ: cx) (e: exp) (e': exp) (τ: typ)
+  (Γ: cx) (Δ: vcx)
+  (cx_nil: Γ = cx.nil)
+  (vcx_nil: Δ = vcx.nil)
+  (e: exp) (e': exp) (τ: typ)
   (et: exp_typ Γ e τ)
-  (is_nil: Γ = cx.nil)
-  (st: steps e e')
+  (st: steps Δ e e')
   : exp_typ Γ e' τ :=
 begin
   induction st generalizing Γ τ,
   let inv := inversion_if Γ st_e1 st_e2 st_e3 τ et,
-  let e1_typ := st_ih Γ typ.bool inv.left is_nil,
+  let e1_typ := st_ih vcx_nil Γ typ.bool cx_nil inv.left,
   exact exp_typ.if_ Γ st_e1' st_e2 st_e3 τ e1_typ inv.right.left inv.right.right,
   let inv := inversion_if Γ (exp.pure val.true) st_e2 st_e3 τ et,
   exact inv.right.left,
