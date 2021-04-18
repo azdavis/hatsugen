28c30ea71a21907337c40b3273e1156b821df550
Ariel Davis <ariel.z.davis@icloud.com>
2021-04-17 17:28:20 -0700

    Rename, rewrite less

--- src/dynamics.lean
+++ src/dynamics.lean
@@ -5,9 +5,9 @@ inductive steps: exp -> exp -> Prop
     (e1: exp) (e1': exp) (e2: exp) (e3: exp):
     steps e1 e1' ->
     steps (exp.if_ e1 e2 e3) (exp.if_ e1' e2 e3)
-| if_e2
+| if_true
     (e2: exp) (e3: exp):
     steps (exp.if_ (exp.pure val.true) e2 e3) e2
-| if_e3
+| if_false
     (e2: exp) (e3: exp):
     steps (exp.if_ (exp.pure val.false) e2 e3) e3
