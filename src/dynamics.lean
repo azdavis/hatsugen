cf1ea37fd9ecfed9fcca0aeefea48de22adb321f
Ariel Davis <ariel.z.davis@icloud.com>
2021-04-19 21:18:56 -0700

    Rm cx exp

--- src/dynamics.lean
+++ src/dynamics.lean
@@ -5,17 +5,14 @@ inductive val: exp -> Prop
 | true: val exp.true
 | false: val exp.false
 
-inductive steps: cx exp -> exp -> cx exp -> exp -> Prop
+inductive steps: exp -> exp -> Prop
 | if_e1
-    (Δ: cx exp) (Δ': cx exp)
     (e1: exp) (e1': exp) (e2: exp) (e3: exp):
-    steps Δ e1 Δ' e1' ->
-    steps Δ (exp.if_ e1 e2 e3) Δ' (exp.if_ e1' e2 e3)
+    steps e1 e1' ->
+    steps (exp.if_ e1 e2 e3) (exp.if_ e1' e2 e3)
 | if_true
-    (Δ: cx exp)
     (e2: exp) (e3: exp):
-    steps Δ (exp.if_ exp.true e2 e3) Δ e2
+    steps (exp.if_ exp.true e2 e3) e2
 | if_false
-    (Δ: cx exp)
     (e2: exp) (e3: exp):
-    steps Δ (exp.if_ exp.false e2 e3) Δ e3
+    steps (exp.if_ exp.false e2 e3) e3
--- src/safety.lean
+++ src/safety.lean
@@ -1,12 +1,11 @@
 import helpers
 
 theorem progress
-  (Γ: cx typ) (Δ: cx exp)
+  (Γ: cx typ)
   (tcx_nil: Γ = list.nil)
-  (vcx_nil: Δ = list.nil)
   (e: exp) (τ: typ)
   (et: has_typ Γ e τ)
-  : val e ∨ (∃ (Δ': cx exp) (e': exp), steps Δ e Δ' e') :=
+  : val e ∨ (∃ (e': exp), steps e e') :=
 begin
   induction et,
   left,
@@ -17,32 +16,29 @@ begin
   exact val.false,
   right,
   cases et_ih_a tcx_nil,
-  -- rewrite h_h at *,
   cases bool_canonical_forms et_Γ et_e1 h et_a,
   rewrite h_1,
-  existsi [Δ, et_e2],
-  exact steps.if_true Δ et_e2 et_e3,
+  existsi et_e2,
+  exact steps.if_true et_e2 et_e3,
   rewrite h_1,
-  existsi [Δ, et_e3],
-  exact steps.if_false Δ et_e2 et_e3,
+  existsi et_e3,
+  exact steps.if_false et_e2 et_e3,
   cases h,
-  cases h_h,
-  existsi [h_w, exp.if_ h_h_w et_e2 et_e3],
-  exact steps.if_e1 Δ h_w et_e1 h_h_w et_e2 et_e3 h_h_h,
+  existsi exp.if_ h_w et_e2 et_e3,
+  exact steps.if_e1 et_e1 h_w et_e2 et_e3 h_h,
 end
 
 theorem preservation
-  (Γ: cx typ) (Δ: cx exp) (Δ': cx exp)
+  (Γ: cx typ)
   (tcx_nil: Γ = list.nil)
-  (vcx_nil: Δ = list.nil)
   (e: exp) (e': exp) (τ: typ)
   (et: has_typ Γ e τ)
-  (st: steps Δ e Δ' e')
+  (st: steps e e')
   : has_typ Γ e' τ :=
 begin
   induction st generalizing Γ τ,
   let inv := inversion_if Γ st_e1 st_e2 st_e3 τ et,
-  let e1_typ := st_ih vcx_nil Γ typ.bool tcx_nil inv.left,
+  let e1_typ := st_ih Γ typ.bool tcx_nil inv.left,
   exact has_typ.if_ Γ st_e1' st_e2 st_e3 τ e1_typ inv.right.left inv.right.right,
   let inv := inversion_if Γ exp.true st_e2 st_e3 τ et,
   exact inv.right.left,
