a65c0a451eec1bb47833429a42ce2999b8896c92
Ariel Davis <ariel.z.davis@icloud.com>
2021-04-20 23:52:03 -0700

    Prove fresh

--- src/lemmas/helpers.lean
+++ src/lemmas/helpers.lean
@@ -38,3 +38,30 @@ begin
   contradiction,
   exact l1_ih l2_a_1,
 end
+
+theorem fresh (xs: list var): ∃ (x: var), ∀ (y ∈ xs), y < x :=
+begin
+  induction xs,
+  existsi 0,
+  intros y h,
+  let notin := list.not_mem_nil y,
+  contradiction,
+  cases xs_ih,
+  cases lt_trichotomy xs_hd xs_ih_w,
+  existsi xs_ih_w,
+  intros y h,
+  cases h,
+  rw h_1,
+  exact h,
+  exact xs_ih_h y h_1,
+  existsi nat.succ xs_hd,
+  intros y h,
+  cases h,
+  rw h_1,
+  exact nat.lt_succ_of_le (nat.less_than_or_equal.refl xs_hd),
+  let y_lt_w := xs_ih_h y h_1,
+  cases h,
+  rw h,
+  exact nat.lt_succ_of_le (le_not_le_of_lt y_lt_w).left,
+  exact nat.lt_succ_of_le (le_of_lt (lt_trans y_lt_w h)),
+end
