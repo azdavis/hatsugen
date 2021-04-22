a86f27dcb44c7775b1a5a7c336d31865a5daf471
Ariel Davis <ariel.z.davis@icloud.com>
2021-04-21 22:30:50 -0700

    Do some easy cases

--- src/lemmas/inversion.lean
+++ src/lemmas/inversion.lean
@@ -12,3 +12,15 @@ begin
   exact et_a_1,
   exact et_a_2,
 end
+
+theorem inversion_app
+  {Γ: cx typ} {e1 e2: exp} {τ2: typ}
+  (et: has_typ Γ (exp.app e1 e2) τ2)
+  : ∃ (τ1: typ), has_typ Γ e1 (typ.arrow τ1 τ2) ∧ has_typ Γ e2 τ1 :=
+begin
+  cases et,
+  existsi et_τ1,
+  split,
+  exact et_a,
+  exact et_a_1,
+end
