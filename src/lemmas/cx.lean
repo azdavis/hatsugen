f23e55d5f3ff87100d0ba951870501c0e7e24d5b
Ariel Davis <ariel.z.davis@icloud.com>
2021-05-08 03:35:24 -0700

    Make it an iff

--- src/lemmas/cx.lean
+++ src/lemmas/cx.lean
@@ -1,12 +1,13 @@
 import defs.var
 
 theorem lookup_mem_entries {t: Type} (Γ: cx t) (x: var) (v: t):
-  cx_elem.mk x v ∈ Γ.entries ->
+  cx_elem.mk x v ∈ Γ.entries ↔
   cx.lookup Γ x = some v :=
 begin
   cases Γ,
+  simp,
+  split,
   intro h,
-  simp at h,
   induction Γ_entries,
   exfalso,
   exact list.not_mem_nil (cx_elem.mk x v) h,
@@ -23,6 +24,22 @@ begin
   simp [cx.lookup],
   simp [a'],
   exact Γ_entries_ih Γ_nodupkeys_a_1 h,
+  intro h,
+  induction Γ_entries,
+  simp [cx.lookup] at h,
+  exact h,
+  simp [cx.lookup] at h,
+  cases decidable.em (x = Γ_entries_hd.x),
+  simp [h_1] at h,
+  left,
+  cases Γ_entries_hd,
+  simp at h h_1,
+  rw h_1,
+  rw symm h,
+  simp [h_1] at h,
+  cases Γ_nodupkeys,
+  right,
+  exact Γ_entries_ih Γ_nodupkeys_a_1 h,
 end
 
 theorem lookup_insert {t: Type} (Γ: cx t) (x: var) (v: t):
@@ -36,7 +53,7 @@ begin
   let ys' := insertion_sort ys,
   let h': elem ∈ ys' := insertion_sort_mem elem h,
   simp [cx.insert],
-  exact lookup_mem_entries (cx.mk ys' _) x v h',
+  exact iff.elim_left (lookup_mem_entries (cx.mk ys' _) x v) h',
 end
 
 theorem useless_insert_ne {t: Type} (Γ: cx t) (x y: var) (v: t):
