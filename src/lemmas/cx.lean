b7f29ed723cb19f8c5011a340c14e77e85ac38a5
Ariel Davis <ariel.z.davis@icloud.com>
2021-05-08 13:44:39 -0700

    Prove lookup_mem_entries_ne

--- src/lemmas/cx.lean
+++ src/lemmas/cx.lean
@@ -42,6 +42,34 @@ begin
   exact Γ_entries_ih Γ_nodupkeys_a_1 h,
 end
 
+theorem lookup_mem_entries_ne {t: Type} (Γ: cx t) (x y: var) (vx vy: t):
+  x ≠ y ->
+  (cx_elem.mk x vx ∈ Γ.entries ↔
+  cx_elem.mk x vx ∈ (cx.insert y vy Γ).entries) :=
+begin
+  intro xy_ne,
+  let p := fun (a: cx_elem t), ¬y = a.x,
+  let elem := cx_elem.mk x vx,
+  let p_elem: p elem := begin
+    simp [p],
+    simp [elem],
+    exact fun b, xy_ne (symm b),
+  end,
+  cases Γ,
+  simp,
+  split,
+  intro h,
+  let a: elem ∈ list.filter p Γ_entries :=
+    iff.elim_right (filter_spec p Γ_entries elem) (and.intro h p_elem),
+  exact iff.elim_left insertion_sort_mem (or.inr a),
+  intro h,
+  cases iff.elim_right insertion_sort_mem h,
+  cases cx_elem.mk.inj h_1,
+  exfalso,
+  exact xy_ne left,
+  exact (iff.elim_left (filter_spec p Γ_entries elem) h_1).left,
+end
+
 theorem lookup_insert {t: Type} (Γ: cx t) (x: var) (v: t):
   cx.lookup (cx.insert x v Γ) x = some v :=
 begin
